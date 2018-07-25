2c08fe64c070e7878af50fcd419c0948ddcf33ff
ArnoldPieterse <apieterse91@gmail.com>
2018-07-25 23:41:25 +1000

    added Flight model and api calls

--- app/src/main/java/com/example/apiet/flightcentrekotlintest/activity/model/Flight.kt
+++ app/src/main/java/com/example/apiet/flightcentrekotlintest/activity/model/Flight.kt
@@ -1,4 +1,3 @@
 package com.example.apiet.flightcentrekotlintest.activity.model
 
-class Flight {
-}
+data class Flight(val id : Int, val departure_date : String, val airline_code : String, val flight_number : String, val departure_airport : String, val departure_city : String, val arrival_city : String, val arrival_airport : String, val scheduled_duration : String, val arrival_date : String, val created_at : String, val updated_at : String)
--- app/src/main/java/com/example/apiet/flightcentrekotlintest/activity/service/Api.kt
+++ app/src/main/java/com/example/apiet/flightcentrekotlintest/activity/service/Api.kt
@@ -1,4 +1,14 @@
 package com.example.apiet.flightcentrekotlintest.activity.service
 
+import com.example.apiet.flightcentrekotlintest.activity.model.Flight
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface Api {
+    @GET("all")
+    fun getAllFlights() : Observable<List<Flight>>
+
+    @GET("{id}")
+    fun getFlightById(@Path("id") id: Int) : Observable<Flight>
 }
